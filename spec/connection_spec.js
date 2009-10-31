af6a7a14e7e201189eab00effc3bc2545f1cf497
Anders Conbere <aconbere@gmail.com>
2009-10-30 14:51:34 -1000

    huge number of tests finished

--- spec/connection_spec.js
+++ spec/connection_spec.js
@@ -1,2 +1,86 @@
+var MockConnection = function(host) {
+    this.host = host;
+    this.handlers = [];
+};
+
+MockConnection.prototype.connect = function(jid, password, on_status_change){
+    this.jid = jid;
+    this.password = password;
+    this.on_status_change = on_status_change;
+
+    this.status_to_connected = [Strophe.Status.CONNECTING, Strophe.Status.CONNECTED];
+    for(var i = 0; i < this.status_to_connected.length; i++) {
+        this.on_status_change(this.status_to_connected[i]);
+    }
+};
+
+MockConnection.prototype.disconnect = function(){
+    this.status_to_disconnected = [Strphe.Status.DISCONNECTING, Strophe.Strophe.DISCONNECTED];
+
+    for(var i = 0; i < this.status_to_connected.length; i++) {
+        this.on_status_change(this.status_to_connected[i]);
+    }
+};
+
+MockConnection.prototype.addHandler = function(func){
+    this.handlers.push(func);
+};
+
+MockConnection.prototype.send = function(s){};
+
+MockHandler = function(){
+    this._on_stanza = false;
+    this._on_connecting = false;
+    this._on_disconnecting = false;
+    this._on_disconnected = false;
+    this._on_connection_failed = false;
+    this._on_connected = false;
+};
+MockHandler.prototype.on_stanza = function(s){ this._on_stanza = true;}, 
+MockHandler.prototype.on_connecting = function(){ this._on_connecting = true };
+MockHandler.prototype.on_disconnecting = function(){ this._on_disconnecting = true };
+MockHandler.prototype.on_disconnected = function(){ this._on_disconnected = true };
+MockHandler.prototype.on_connection_failed = function(){ this._on_connection_failed = true };
+MockHandler.prototype.on_connected = function(conn){ this.conn = conn;
+                                                     this._on_connected = true; };
+
 // Needs a Mock Strophe Connection
-Screw.Unit(function() {});
+Screw.Unit(function() {
+    describe("Babylon.Connection", function() {
+        var test_host = "test_host";
+        var jid = "student@hth.com";
+        var password = "password";
+        var connection = new Object();
+        var test_handler = new Object();
+        Strophe.Connection = MockConnection;
+
+        before(function(){
+            test_handler = new MockHandler();
+            connection = new Babylon.Connection(test_host, test_handler);
+        });
+
+        describe("init", function() {
+            it("should set the connected, host, connection, handler", function(){
+                expect(connection.connected).to(equal, false);
+                expect(connection.host).to(equal, test_host);
+                expect(connection.connection).to_not(equal, null);
+                expect(connection.handler).to(equal, test_handler);
+            });
+        });
+
+        describe("connect", function() {
+            it("should set the jid and password", function(){
+                connection.connect(jid, password);
+                expect(connection.jid).to(equal, jid);
+                expect(connection.password).to(equal, password);
+            }); 
+
+            it("should trigger, connecting and connected callbacks", function(){
+            });
+        });
+
+        describe("disconnect", function() {
+            it("should set connected to false", function(){});
+        });
+    });
+});
--- spec/controller_spec.js
+++ spec/controller_spec.js
@@ -3,31 +3,91 @@ Screw.Unit(function() {
         var controller = new Object();
         var action = "";
         var name = "";
+
         before(function(){
             action = "test-action";
             name = "test-controller";
-            controller = function(stanza){this.stanza = stanza; this.name = name} 
-            controller.prototype = new Babylon.Controller();
+
+            controller = new Babylon.Controller();
+            controller.name = name;
             controller[action] = function(){};
+
+            Babylon.Views[name] = {}
+            Babylon.Views[name][action] = function(){ return "default view"};
         });
 
         describe("perform", function() {
-            it("should set the action parameter", function() {
+            before(function() {
+                controller = new Babylon.Controller();
+                controller.name = name;
+                controller[action] = function(){};
+            });
+
+            it("should set the action_name parameter", function() {
                 controller.perform(action);
-                expect(controller.action).to(equal, action);
+                expect(controller.action_name).to(equal, action);
+            });
+
+            it("should call the action specified", function() {
+                controller[action] = function(){ this.perform_action_set_var = "xyz" };
+                controller.perform(action);
+                expect(controller.perform_action_set_var).to(equal, "xyz");
+            });
+
+            it("shouuld render the view attached to the action", function() {
+                expect(controller.perform(action)()).to(equal, "default view");
             });
         });
 
         describe("render", function() {
-            it("should set this.view to a callable", function(){});
-            it("should return null if this.view is already set", function(){});
-            it("should call a different action if the action option is supplied", function(){});
-            it("should set this.view to a callable that returns null, if the nothing option is supplied", function(){});
+            before(function(){
+                controller = new Babylon.Controller();
+                controller.name = name;
+                controller[action] = function(){};
+            });
+            describe("when render has been called once before", function() {
+                before(function() {
+                    controller.render({view: true});
+                });
+                it("should return null", function(){
+                    expect(controller.render()).to(equal, null);
+                });
+            });
+            describe("when {nothing: true} is passed in", function() {
+                it("should set this.view to a callable that returns \"\"", function(){
+                    expect(controller.render({nothing: true})()).to(equal, "");
+                });
+            });
+
+            describe("when {view: xxx} is passed in", function(){
+                it("should set this.view to that function", function() {
+                    expect(controller.render({view: function(l){return "test"}})()).to(equal, "test");
+                });
+            });
+
+            describe("when {action: xxx} is passed in", function(){
+                before(function() {
+                    controller["alt-action"] = function(){};
+                    Babylon.Views[name]["alt-action"] = function(){ return "abcd" };
+                });
+
+                it("should pass control to action xxx", function() {
+                    expect(controller.render({action: "alt-action"})()).to(equal, "abcd");
+                });
+            });
         });
 
         describe("evaluate", function() {
-            it("should return \"\" if this.view is unset", function(){});
-            it("should return the result of calling this.view", function(){});
+            it("should return \"\" if this.view is unset", function(){
+                controller.view = null;
+                expect(controller.evaluate()).to(equal, "");
+            });
+
+            it("should return the result of calling the view in that scope", function(){
+                controller.view = function(bind){ return bind.bound_var };
+                controller.bound_var = "tada";
+                expect(controller.evaluate()).to(equal, "tada");
+            });
         });
     });
 });
